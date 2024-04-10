dad4afa0f94a9252d9dba8d4d42f8d057d009f47
Michael Thessel <michael.thessel@gmx.de>
2024-04-09 22:28:57 -0700

    Moved album generator to service

--- cmd/generate/generate.go
+++ cmd/generate/generate.go
@@ -1,16 +1,13 @@
 package main
 
 import (
-	"html/template"
-	"log"
-	"os"
-
 	"github.com/MichaelThessel/photo/model"
+	"github.com/MichaelThessel/photo/service"
 )
 
 func main() {
-	ag := AlbumGenerator{
-		exportPath: "./data",
+	ag := service.AlbumGenerator{
+		ExportPath: "./data",
 	}
 
 	tree := model.NewTree()
@@ -22,42 +19,4 @@ func main() {
 		}
 	}
 
-}
-
-type AlbumGenerator struct {
-	exportPath string
-}
-
-func (ag *AlbumGenerator) GenerateAlbumHTML(folder model.Folder, album model.Album) {
-	albumPath := ag.exportPath + "/" + folder.Name + "/" + album.Name
-
-	f, err := os.Create(albumPath + "/index.html")
-	if err != nil {
-		log.Println("Coudn't open album index file", err)
-	}
-	defer f.Close()
-
-	tmpl, err := template.ParseFiles(
-		"templates/album.html",
-		"templates/head.html",
-		"templates/header.html",
-		"templates/footer.html",
-	)
-	if err != nil {
-		log.Panic("Coudn't open template files", err)
-	}
-
-	templateData := struct {
-		Page  model.Page
-		Album model.Album
-	}{
-		Page: model.Page{
-			Title: album.Name,
-		},
-		Album: album,
-	}
-	err = tmpl.Execute(f, templateData)
-	if err != nil {
-		log.Panic("Coudn't compile template", err)
-	}
 }
---
+++ service/album_generator.go
@@ -0,0 +1,47 @@
+package service
+
+import (
+	"html/template"
+	"log"
+	"os"
+
+	"github.com/MichaelThessel/photo/model"
+)
+
+type AlbumGenerator struct {
+	ExportPath string
+}
+
+func (ag *AlbumGenerator) GenerateAlbumHTML(folder model.Folder, album model.Album) {
+	albumPath := ag.ExportPath + "/" + folder.Name + "/" + album.Name
+
+	f, err := os.Create(albumPath + "/index.html")
+	if err != nil {
+		log.Println("Coudn't open album index file", err)
+	}
+	defer f.Close()
+
+	tmpl, err := template.ParseFiles(
+		"templates/album.html",
+		"templates/head.html",
+		"templates/header.html",
+		"templates/footer.html",
+	)
+	if err != nil {
+		log.Panic("Coudn't open template files", err)
+	}
+
+	templateData := struct {
+		Page  model.Page
+		Album model.Album
+	}{
+		Page: model.Page{
+			Title: album.Name,
+		},
+		Album: album,
+	}
+	err = tmpl.Execute(f, templateData)
+	if err != nil {
+		log.Panic("Coudn't compile template", err)
+	}
+}
